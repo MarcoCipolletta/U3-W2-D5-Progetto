f7346cf20d7c410e308307fb4e560702fe928891
MarcoCipolletta <marcocipolletta1998@gmail.com>
2024-10-18 17:03:31 +0200

    Set random time to card

--- src/app/componets/card-todo/card-todo.component.ts
+++ src/app/componets/card-todo/card-todo.component.ts
@@ -9,4 +9,16 @@ import { iTodo } from '../../interfaces/iTodo';
 export class CardTodoComponent {
   @Input() todo!: iTodo;
   @Input() inputID!: string;
+
+  get randomTime(): string {
+    const hours = Math.floor(Math.random() * 12) + 1;
+
+    const minutes = ['00', '15', '30', '45'];
+
+    const amOrPm = Math.random() < 0.5 ? 'AM' : 'PM';
+
+    const formattedMinutes = minutes[Math.floor(Math.random() * 4)];
+
+    return `${hours}:${formattedMinutes} ${amOrPm}`;
+  }
 }
